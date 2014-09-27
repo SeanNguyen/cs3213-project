d2fefeeab76d29a68990a44d5a2af981ecb1f58c
sudarsangp <sudarsangp@gmail.com>
2014-09-27 17:40:23 +0800

    separated the move buttons to make the process faster. when save clicked the data in the workspace main in printed on the console

--- js/workspace.js
+++ js/workspace.js
@@ -15,4 +15,18 @@ $(document).ready(function(){
             $("#workspace-main .workspace-row").first().clone().appendTo($(this).parent().parent()).removeClass("hidden");
         }
     });
+
+    $('#save-data').on('click', function(){
+        var dataObjects = $('#workspace-main').find('.input-group.command-object');
+        var arrayObjects = [];
+        var dataFormat = {};
+        $.each(dataObjects, function(index, value){
+           dataFormat['data-type'] = $(value).attr('data-type');
+           dataFormat['data-direction'] = $(value).attr('data-direction');
+           dataFormat['data-steps'] = $(value).attr('data-steps');
+           arrayObjects.push(dataFormat);
+        });
+        console.log(dataObjects.length);
+        console.log(arrayObjects);
+    });
 });
